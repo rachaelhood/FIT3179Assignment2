681877b8efbfa97b7c130a10d964ec06b77b7dc8
rachaelhood <rhoo0016@student.monash.edu>
2025-10-15 12:17:04 +1100

    Deleting fit3179-project-starter

--- fit3179-project-starter/js/main.js
+++
@@ -1,6 +0,0 @@
-/* Embed Vega-Lite specs */
-const embedOpt = {actions: false};
-
-vegaEmbed("#map", "./specs/africa_forest_map.vg.json", embedOpt);
-
-vegaEmbed("#behaviour", "./specs/wildlife_behaviour.vg.json", embedOpt);
