580ae0b0350ca7ff1bd3e18772f96ff492ca4dc2
rachaelhood <rhoo0016@student.monash.edu>
2025-10-15 12:19:03 +1100

    Update js/main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
-const embedOpt = { actions: false };
+/* Embed Vega-Lite specs */
+const embedOpt = {actions: false};
 
 vegaEmbed("#map", "./specs/africa_forest_map.vg.json", embedOpt);
+
 vegaEmbed("#behaviour", "./specs/wildlife_behaviour.vg.json", embedOpt);
